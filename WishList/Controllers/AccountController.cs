beaee2df4d3056e26e01e6fdda8eac79efc518ee
Ajaimini <er.ajaysoni555@gmial.com>
2022-05-25 20:49:58 +0530

    Create HttpPost Register Action Add ModelState Check To HttpPost Register Action
    Call CreateAsync In HttpPost Register Action
    
    Check CreateAsync Result HttpPost Register Action

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using WishList.Models;
 using WishList.Models.AccountViewModels;
 
+
 namespace WishList.Controllers
 {
     [Authorize]
@@ -28,6 +29,21 @@ namespace WishList.Controllers
         {
             return View("Register");
         }
-        
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var result=_userManager.CreateAsync(new ApplicationUser() { Email = model.Email, UserName = model.Email }, model.Password).Result;
+            if (!result.Succeeded)
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("Password",error.Description);
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
